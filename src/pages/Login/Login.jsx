dc2fa959a90c17bc800da7cd36780a41e71bca68
Gabriele <dotigabriele@gmail.com>
2025-06-28 14:38:30 +0200

    logo

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Login.css';
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <div className="login-logo">
+          <img src="/logo_capitalia.png" alt="Logo Capitalia" />
+        </div>
+
+        <h2 className="login-title">LOGIN</h2>
+
+        <form className="login-form">
+          <div className="form-group">
+            <label>Username o Email</label>
+            <input type="text" />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" />
+          </div>
+
+          <button type="submit" className="login-button">Accedi</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
