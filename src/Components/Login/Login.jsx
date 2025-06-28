90e463487875a944b04e579d17cceeea0259496e
Gabriele <dotigabriele@gmail.com>
2025-06-28 15:38:27 +0200

    fix .env

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Login.css';
 
+const apiDbUrl = import.meta.env.VITE_API_DB_URL;
+
 export default function Login() {
   return (
     <div className="login-container">
